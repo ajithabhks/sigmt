1c290b4f5fde6c0bf5e0f7656d78325610994a17
Ajithabh K.S. <ajithabhks@gmail.com>
2025-05-30 19:04:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author='Ajithabh K.S.',
     author_email='ajithabhks@gmail.com',
     description='A tool for magnetotelluric time series processing',
-    long_description=open('README.md').read(),
+    long_description=open('README.md, encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/ajithabhks/sigmt',
     license='GPL',
