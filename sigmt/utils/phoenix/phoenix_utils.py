c29d6d28c990b12edd11801145680c32aa385abc
Ajithabh Kalari Sudhakaran <ajithabhks@gmail.com>
2026-01-24 21:41:59 -0600

    refactor and optimization

--- sigmt/utils/phoenix/phoenix_utils.py
+++ sigmt/utils/phoenix/phoenix_utils.py
@@ -175,75 +175,166 @@ def list_unique_td_extensions(
 
 
 def read_decimated_continuous_data(
-        recording_path,
-        channel_map,
-        file_extension
+        recording_path: str,
+        channel_map: Dict,
+        file_extension: str
 ) -> Dict:
+    """
+    Read decimated continuous data.
+
+    :param recording_path: Path to the recording folder
+    :type recording_path: str
+    :param channel_map: Channel map dict
+    :type channel_map: Dict
+    :param file_extension: File extension of file
+    :type file_extension: str
+    :return: Dictionary containing time series data
+    :rtype: Dict
+
+    """
     recording_path = pathlib.Path(recording_path)
     ts = {
         'run0': {}
     }
 
-    ts['run0']['ex'] = phoenix_readers.read_decimated_continuous(
-        channel_path=recording_path / str(channel_map.get('E1')),
-        file_extension=file_extension,
-    )
+    ex_map = channel_map.get('E1', None)
+    if ex_map is not None:
+        channel_path = recording_path / str(ex_map)
+        if channel_path.exists():
+            ts["run0"]["ex"] = phoenix_readers.read_decimated_continuous(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+        else:
+            print(f"Warning: channel path does not exist: {channel_path}")
 
-    ts['run0']['ey'] = phoenix_readers.read_decimated_continuous(
-        channel_path=recording_path / str(channel_map.get('E2')),
-        file_extension=file_extension,
-    )
+    ey_map = channel_map.get('E2', None)
+    if ey_map is not None:
+        channel_path = recording_path / str(ey_map)
+        if channel_path.exists():
+            ts['run0']['ey'] = phoenix_readers.read_decimated_continuous(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+        else:
+            print(f"Warning: channel path does not exist: {channel_path}")
 
-    ts['run0']['hx'] = phoenix_readers.read_decimated_continuous(
-        channel_path=recording_path / str(channel_map.get('H1')),
-        file_extension=file_extension,
-    )
+    hx_map = channel_map.get('H1', None)
+    if hx_map is not None:
+        channel_path = recording_path / str(hx_map)
+        if channel_path.exists():
+            ts['run0']['hx'] = phoenix_readers.read_decimated_continuous(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+        else:
+            print(f"Warning: channel path does not exist: {channel_path}")
 
-    ts['run0']['hy'] = phoenix_readers.read_decimated_continuous(
-        channel_path=recording_path / str(channel_map.get('H2')),
-        file_extension=file_extension,
-    )
+    hy_map = channel_map.get('H2', None)
+    if hy_map is not None:
+        channel_path = recording_path / str(hy_map)
+        if channel_path.exists():
+            ts['run0']['hy'] = phoenix_readers.read_decimated_continuous(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+        else:
+            print(f"Warning: channel path does not exist: {channel_path}")
 
-    ts['run0']['hz'] = phoenix_readers.read_decimated_continuous(
-        channel_path=recording_path / str(channel_map.get('H3')),
-        file_extension=file_extension,
-    )
+    hz_map = channel_map.get('H3', None)
+    if hz_map is not None:
+        channel_path = recording_path / str(hz_map)
+        if channel_path.exists():
+            ts['run0']['hz'] = phoenix_readers.read_decimated_continuous(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+        else:
+            print(f"Warning: channel path does not exist: {channel_path}")
 
     return ts
 
 
-def read_decimated_segmented_data(recording_path, channel_map, file_extension) -> Dict:
+def read_decimated_segmented_data(
+        recording_path: str,
+        channel_map: Dict,
+        file_extension: str,
+) -> Dict:
+    """
+    Read decimated segmented data
+
+    :param recording_path: Path to recordings folder
+    :type recording_path: str
+    :param channel_map: Dict of channel map
+    :type channel_map: dict
+    :param file_extension: File extension
+    :type file_extension: str
+    :return: Dict of time series
+    :rtype: dict
+
+    """
     recording_path = pathlib.Path(recording_path)
-    ts = {}
+    ts: Dict[str, Dict[str, Any]] = {}
 
-    def add_channel(chan_key: str, field: str) -> int:
-        ch = channel_map.get(chan_key)
-        if ch is None:
-            raise KeyError(f"channel_map missing key {chan_key}")
+    ex_map = channel_map.get('E1', None)
+    if ex_map is not None:
+        channel_path = recording_path / str(ex_map)
+        if channel_path.exists():
+            segments = phoenix_readers.read_decimated_segmented(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+            for num, list_item in enumerate(segments):
+                run_key = f"run{num}"
+                ts.setdefault(run_key, {})["ex"] = list_item.get("samples")
 
-        out_list = phoenix_readers.read_decimated_segmented(
-            channel_path=recording_path / str(ch),
-            file_extension=file_extension,
-        )
+    ey_map = channel_map.get('E2', None)
+    if ey_map is not None:
+        channel_path = recording_path / str(ey_map)
+        if channel_path.exists():
+            segments = phoenix_readers.read_decimated_segmented(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+            for num, list_item in enumerate(segments):
+                run_key = f"run{num}"
+                ts.setdefault(run_key, {})["ey"] = list_item.get("samples")
 
-        for num, out_val in enumerate(out_list):
-            run_key = f"run{num}"
-            run = ts.setdefault(run_key, {})
-            run[field] = out_val["samples"]
+    hx_map = channel_map.get('H1', None)
+    if hx_map is not None:
+        channel_path = recording_path / str(hx_map)
+        if channel_path.exists():
+            segments = phoenix_readers.read_decimated_segmented(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+            for num, list_item in enumerate(segments):
+                run_key = f"run{num}"
+                ts.setdefault(run_key, {})["hx"] = list_item.get("samples")
 
-        return len(out_list)
+    hy_map = channel_map.get('H2', None)
+    if hy_map is not None:
+        channel_path = recording_path / str(hy_map)
+        if channel_path.exists():
+            segments = phoenix_readers.read_decimated_segmented(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+            for num, list_item in enumerate(segments):
+                run_key = f"run{num}"
+                ts.setdefault(run_key, {})["hy"] = list_item.get("samples")
 
-    counts = {
-        "E1/ex": add_channel("E1", "ex"),
-        "E2/ey": add_channel("E2", "ey"),
-        "H1/hx": add_channel("H1", "hx"),
-        "H2/hy": add_channel("H2", "hy"),
-        "H3/hz": add_channel("H3", "hz"),
-    }
-
-    if len(set(counts.values())) != 1:
-        raise ValueError(f"Run count mismatch across channels: {counts}")
-
+    hz_map = channel_map.get('H3', None)
+    if hz_map is not None:
+        channel_path = recording_path / str(hz_map)
+        if channel_path.exists():
+            segments = phoenix_readers.read_decimated_segmented(
+                channel_path=channel_path,
+                file_extension=file_extension,
+            )
+            for num, list_item in enumerate(segments):
+                run_key = f"run{num}"
+                ts.setdefault(run_key, {})["hz"] = list_item.get("samples")
     return ts
 
 
@@ -254,7 +345,18 @@ def optimize_time_series_dict(
         max_runs: Optional[int] = 20,
 ) -> Dict:
     """
-    Optimized time series dictionary
+    Optimized time series dictionary. It reduces number of runs.
+
+    :param time_series_dict: Dictionary of time series
+    :type time_series_dict: dict
+    :param fft_length: FFT Length
+    :type fft_length: int
+    :param overlap: Overlap in percentage
+    :type overlap: int
+    :param max_runs: Number of runs reduced to
+    :type max_runs: int
+    :return: Optimized time series dict
+    :rtype: dict
     """
     print('Optimizing time series dictionary')
 
@@ -299,7 +401,14 @@ def optimize_time_series_dict(
     return reduced_time_series
 
 
-def extract_bbbbbbbb(filename: str):
+def extract_bbbbbbbb(filename: str) -> str:
+    """
+    Extract time stamp from filename
+    :param filename: filename
+    :type filename: str
+    :return: string
+    :rtype: str
+    """
     parts = filename.split("_")
     if len(parts) < 4:
         return None
@@ -309,11 +418,22 @@ def extract_bbbbbbbb(filename: str):
     return None
 
 
-def list_files_by_bbbbbbbb_first_existing_channel(base_path, file_extension,
-                                                  channels=(0, 1, 2, 3, 4)):
+def list_files_by_bbbbbbbb_first_existing_channel(
+        base_path: str,
+        file_extension: str,
+        channels: tuple = (0, 1, 2, 3, 4)
+) -> tuple:
     """
     Search ONLY the first existing channel folder among 0..4.
     Returns: (result_dict, chosen_channel or None)
+
+    :param base_path: Path to folder
+    :type base_path: str
+    :param file_extension: File extension
+    :type file_extension: str
+    :param channels: Channels
+    :type channels: tuple
+
     """
     suffix = f".{file_extension}" if not file_extension.startswith(".") else file_extension
     base = Path(base_path)
@@ -332,8 +452,12 @@ def list_files_by_bbbbbbbb_first_existing_channel(base_path, file_extension,
     return defaultdict(list), None
 
 
-def return_overlapping_info(file_extension, local_station_path, remote_station_path,
-                            channels=(0, 1, 2, 3, 4)):
+def return_overlapping_info(
+        file_extension,
+        local_station_path,
+        remote_station_path,
+        channels=(0, 1, 2, 3, 4)
+):
     local_map, local_ch = list_files_by_bbbbbbbb_first_existing_channel(local_station_path,
                                                                         file_extension, channels)
     remote_map, remote_ch = list_files_by_bbbbbbbb_first_existing_channel(remote_station_path,
@@ -348,6 +472,9 @@ def prepare_calibration_data_electric(
         local_recmeta_data: Dict,
         channel_map: Dict
 ) -> Dict:
+    """
+    Prepare electric calibration data.
+    """
     calibration_data_electric = {
         'ex': {},
         'ey': {},
